1b0d162469194cee3ffb3c50d7db175c3e8ed808
ujszaszik <ujszaszi.karesz@gmail.com>
2025-02-01 22:18:40 +0100

    Feature: App - Use every feature inside App Screen

--- app/src/main/java/hu/ujszaszik/moviemaniac/MainActivity.kt
+++ app/src/main/java/hu/ujszaszik/moviemaniac/MainActivity.kt
@@ -4,38 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
+import hu.ujszaszik.moviemaniac.core.network.WebService
+import hu.ujszaszik.moviemaniac.features.app.AppScreen
 import hu.ujszaszik.moviemaniac.ui.theme.MovieManiacTheme
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var webService: WebService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MovieManiacTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(modifier = Modifier.fillMaxSize()) {
+                    AppScreen()
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
